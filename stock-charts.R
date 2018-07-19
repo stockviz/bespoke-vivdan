98b258b8f0753361fd265e923edf5a2a7601ed9f
vivdan <viveganandan@gmail.com>
2018-07-19 13:33:29 +0300

    Update stock-charts.R

--- stock-charts.R
+++ stock-charts.R
@@ -3,7 +3,7 @@
 #change 'lookbackDays' to specify number of historical business days to plot
 #add technical charts to the TA list. See quantmod documentation for possibilities
 
-symbols<-c('HDFC', 'HDFCBANK', 'TATAELXSI', 'BHARATFIN','NESTLEIND','RELIANCE','MARICO','WIPRO','TCS','TATASTEEL','PIDILITIND','COLPAL')
+symbols<-c('HDFC', 'HDFCBANK', 'TATAELXSI', 'BHARATFIN','NESTLEIND','RELIANCE','MARICO','WIPRO','TCS','TATASTEEL','PIDILITIND','COLPAL','ONGC')
 lookbackDays<-220
 
 library('RODBC')
